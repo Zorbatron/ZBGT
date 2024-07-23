f78e33f376f75b2eb304cb1ff1d61b97fed21107
Zorbatron <gabrielsakash@gmail.com>
2024-07-23 01:27:46 -0400

    feat: tooltips

--- src/main/java/com/zorbatron/zbgt/common/items/ZBGTMetaItem.java
+++ src/main/java/com/zorbatron/zbgt/common/items/ZBGTMetaItem.java
@@ -2,6 +2,8 @@ package com.zorbatron.zbgt.common.items;
 
 import static com.zorbatron.zbgt.common.items.ZBGTMetaItems.*;
 
+import gregtech.common.items.behaviors.TooltipBehavior;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 
 import com.zorbatron.zbgt.ZBUtility;
@@ -10,8 +12,6 @@ import gregtech.api.items.metaitem.MetaItem;
 import gregtech.api.items.metaitem.StandardMetaItem;
 
 public class ZBGTMetaItem extends StandardMetaItem {
-
-    public int id = 0;
 
     public ZBGTMetaItem() {
         super();
@@ -24,13 +24,41 @@ public class ZBGTMetaItem extends StandardMetaItem {
 
     @Override
     public void registerSubItems() {
-        DUAL_COVER_LV = addItem(id, "cover.dual_cover.lv");
-        DUAL_COVER_MV = addItem(++id, "cover.dual_cover.mv");
-        DUAL_COVER_HV = addItem(++id, "cover.dual_cover.hv");
-        DUAL_COVER_EV = addItem(++id, "cover.dual_cover.ev");
-        DUAL_COVER_IV = addItem(++id, "cover.dual_cover.iv");
-        DUAL_COVER_LuV = addItem(++id, "cover.dual_cover.luv");
-        DUAL_COVER_ZPM = addItem(++id, "cover.dual_cover.zpm");
-        DUAL_COVER_UV = addItem(++id, "cover.dual_cover.uv");
+        //Dual Covers: 0-12
+        DUAL_COVER_LV = addItem(0, "cover.dual_cover.lv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate", 8, 1280 / 20));
+        }));
+        DUAL_COVER_MV = addItem(1, "cover.dual_cover.mv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate", 32, 1280 * 4 / 20));
+        }));
+        DUAL_COVER_HV = addItem(2, "cover.dual_cover.hv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate", 64, 1280 * 16 / 20));
+        }));
+        DUAL_COVER_EV = addItem(3, "cover.dual_cover.ev").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate.stacks", 3, 1280 * 64 / 20));
+        }));
+        DUAL_COVER_IV = addItem(4, "cover.dual_cover.iv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate.stacks", 8, 1280 * 64 * 4 / 20));
+        }));
+        DUAL_COVER_LuV = addItem(5, "cover.dual_cover.luv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate.stacks", 16, 1280 * 64 * 16 / 20));
+        }));
+        DUAL_COVER_ZPM = addItem(6, "cover.dual_cover.zpm").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate.stacks", 16, 1280 * 64 * 64 / 20));
+        }));
+        DUAL_COVER_UV = addItem(7, "cover.dual_cover.uv").addComponents(new TooltipBehavior(lines -> {
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip"));
+            lines.add(I18n.format("metaitem.cover.dual_cover.tooltip.transfer_rate.stacks", 16, 1280 * 64 * 64 * 4 / 20));
+        }));
+        //8-12 reserved for higher tier dual covers.
+
+        //Precise Dual Covers: 13-25
     }
 }
