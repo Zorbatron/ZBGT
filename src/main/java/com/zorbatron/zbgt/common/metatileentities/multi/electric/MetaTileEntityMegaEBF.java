930d88058e2dcbf139b8d58bb46bd7224e8c8b54
Zorbatron <gabrielsakash@gmail.com>
2024-09-03 18:02:49 -0400

    Init

---
+++ src/main/java/com/zorbatron/zbgt/common/metatileentities/multi/electric/MetaTileEntityMegaEBF.java
@@ -0,0 +1,6 @@
+package com.zorbatron.zbgt.common.metatileentities.multi.electric;
+
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+
+public class MetaTileEntityMegaEBF extends RecipeMapMultiblockController {
+}
