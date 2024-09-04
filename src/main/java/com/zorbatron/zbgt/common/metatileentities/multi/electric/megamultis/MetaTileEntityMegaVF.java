cdc8dee7046b63041aeb4c3689235043ae26a25c
Zorbatron <gabrielsakash@gmail.com>
2024-09-04 19:29:00 -0400

    Mega vacuum

---
+++ src/main/java/com/zorbatron/zbgt/common/metatileentities/multi/electric/megamultis/MetaTileEntityMegaVF.java
@@ -0,0 +1,98 @@
+package com.zorbatron.zbgt.common.metatileentities.multi.electric.megamultis;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.zorbatron.zbgt.api.metatileentity.LaserCapableGCYMRecipeMapMultiblockController;
+
+import gregicality.multiblocks.api.capability.impl.GCYMMultiblockRecipeLogic;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+
+public class MetaTileEntityMegaVF extends LaserCapableGCYMRecipeMapMultiblockController {
+
+    public MetaTileEntityMegaVF(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.VACUUM_RECIPES);
+        this.recipeMapWorkable = new GCYMMultiblockRecipeLogic(this);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityMegaVF(metaTileEntityId);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle("XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "X#############X",
+                        "X#############X", "X#############X", "X#############X", "X#############X", "XXXXXXXXXXXXXXX")
+                .aisle("XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXSXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX",
+                        "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXX")
+                .where('#', air())
+                .where('S', selfPredicate())
+                .where('X', states(getCasingState())
+                        .or(autoAbilities(false, true, true, true, true, true, false))
+                        .or(autoEnergyInputs(1, 8)))
+                .build();
+    }
+
+    protected IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.ALUMINIUM_FROSTPROOF);
+    }
+
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.FROST_PROOF_CASING;
+    }
+}
