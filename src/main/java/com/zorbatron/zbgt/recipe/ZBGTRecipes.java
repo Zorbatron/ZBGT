c5947ef5688e4282efcad270690b263a772fea49
Zorbatron <gabrielsakash@gmail.com>
2024-09-09 15:16:02 -0400

    Oopsie daisies

--- src/main/java/com/zorbatron/zbgt/recipe/ZBGTRecipes.java
+++ src/main/java/com/zorbatron/zbgt/recipe/ZBGTRecipes.java
@@ -1,12 +1,5 @@
 package com.zorbatron.zbgt.recipe;
 
-import net.minecraft.init.Blocks;
-
-import com.zorbatron.zbgt.api.recipes.ZBGTRecipeMaps;
-import com.zorbatron.zbgt.common.block.blocks.PreciseCasing;
-
-import gregtech.api.GTValues;
-import gregtech.api.unification.material.Materials;
 import gregtech.api.util.Mods;
 
 public class ZBGTRecipes {
@@ -22,13 +15,5 @@ public class ZBGTRecipes {
         if (Mods.AppliedEnergistics2.isModLoaded()) {
             AE2Recipes.init();
         }
-
-        ZBGTRecipeMaps.PRECISE_ASSEMBLER_RECIPES.recipeBuilder()
-                .input(Blocks.DIRT, 64)
-                .fluidInputs(Materials.Water.getFluid(GTValues.L))
-                .output(Blocks.DIAMOND_BLOCK)
-                .CasingTier(PreciseCasing.CasingType.PRECISE_CASING_1.ordinal())
-                .EUt(GTValues.VA[GTValues.MV]).duration(20)
-                .buildAndRegister();
     }
 }
