8d906c27e3735111d547f0af19c70a1e182f8772
Zorbatron <gabrielsakash@gmail.com>
2024-09-09 17:57:28 -0400

    Listen to tech22's advice

--- src/main/java/com/zorbatron/zbgt/api/recipes/ZBGTRecipeMaps.java
+++ src/main/java/com/zorbatron/zbgt/api/recipes/ZBGTRecipeMaps.java
@@ -29,31 +29,10 @@ public final class ZBGTRecipeMaps {
         recipeModList.add(new RecipeIOMod(COMPRESSOR_RECIPES, 0, 0, 1, 1));
 
         recipeModList.forEach(recipeIOMod -> {
-            int recipeMaxItemInputs = recipeIOMod.recipeMap().getMaxInputs();
-            int recipeMaxItemOutputs = recipeIOMod.recipeMap().getMaxOutputs();
-            int recipeMaxFluidInputs = recipeIOMod.recipeMap().getMaxFluidInputs();
-            int recipeMaxFluidOutputs = recipeIOMod.recipeMap().getMaxFluidOutputs();
-
-            int minItemInputs = recipeIOMod.minItemInputs();
-            int minItemOutputs = recipeIOMod.minItemOutputs();
-            int minFluidInputs = recipeIOMod.minFluidInputs();
-            int minFluidOutputs = recipeIOMod.minFluidOutputs();
-
-            if (recipeMaxItemInputs < minItemInputs) {
-                recipeIOMod.recipeMap().setMaxInputs(minItemInputs);
-            }
-
-            if (recipeMaxItemOutputs < minItemOutputs) {
-                recipeIOMod.recipeMap().setMaxOutputs(minItemOutputs);
-            }
-
-            if (recipeMaxFluidInputs < minFluidInputs) {
-                recipeIOMod.recipeMap().setMaxFluidInputs(minFluidInputs);
-            }
-
-            if (recipeMaxFluidOutputs < minFluidOutputs) {
-                recipeIOMod.recipeMap().setMaxFluidOutputs(minFluidOutputs);
-            }
+            recipeIOMod.recipeMap().setMaxInputs(recipeIOMod.minItemInputs());
+            recipeIOMod.recipeMap().setMaxOutputs(recipeIOMod.minItemOutputs());
+            recipeIOMod.recipeMap().setMaxFluidInputs(recipeIOMod.minFluidInputs());
+            recipeIOMod.recipeMap().setMaxFluidOutputs(recipeIOMod.minFluidOutputs());
         });
     }
 }
