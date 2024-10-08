3a9f14b378a6d3404c5fbe19378c14a8a2cf8746
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2024-10-08 13:05:01 -0400

    Actually works now

--- src/main/java/com/zorbatron/zbgt/common/metatileentities/multi/electric/large/MetaTileEntityLargeAlloySmelter.java
+++ src/main/java/com/zorbatron/zbgt/common/metatileentities/multi/electric/large/MetaTileEntityLargeAlloySmelter.java
@@ -1,7 +1,11 @@
 package com.zorbatron.zbgt.common.metatileentities.multi.electric.large;
 
+import java.util.List;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -11,18 +15,23 @@ import com.zorbatron.zbgt.api.render.ZBGTTextures;
 import com.zorbatron.zbgt.common.block.ZBGTMetaBlocks;
 import com.zorbatron.zbgt.common.block.blocks.MiscCasing;
 
+import gregicality.multiblocks.api.capability.impl.GCYMMultiblockRecipeLogic;
 import gregicality.multiblocks.api.metatileentity.GCYMRecipeMapMultiblockController;
 import gregicality.multiblocks.common.block.GCYMMetaBlocks;
 import gregicality.multiblocks.common.block.blocks.BlockUniqueCasing;
-import gregtech.api.capability.impl.MultiblockRecipeLogic;
+import gregtech.api.block.IHeatingCoilBlockStats;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockDisplayText;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.recipes.recipeproperties.IRecipePropertyStorage;
+import gregtech.api.util.GTUtility;
+import gregtech.api.util.TextComponentUtil;
 import gregtech.client.renderer.ICubeRenderer;
 
 public class MetaTileEntityLargeAlloySmelter extends GCYMRecipeMapMultiblockController {
@@ -62,24 +71,84 @@ public class MetaTileEntityLargeAlloySmelter extends GCYMRecipeMapMultiblockCont
         return GCYMMetaBlocks.UNIQUE_CASING.getState(BlockUniqueCasing.UniqueCasingType.HEAT_VENT);
     }
 
+    @Override
+    public boolean hasMufflerMechanics() {
+        return true;
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+
+        Object type = context.get("CoilType");
+        if (type instanceof IHeatingCoilBlockStats) {
+            this.coilTier = ((IHeatingCoilBlockStats) type).getTier();
+        } else {
+            this.coilTier = 0;
+        }
+    }
+
+    @Override
+    public void invalidateStructure() {
+        super.invalidateStructure();
+
+        this.coilTier = -1;
+    }
+
+    @Override
+    protected void addDisplayText(List<ITextComponent> textList) {
+        MultiblockDisplayText.builder(textList, isStructureFormed())
+                .setWorkingStatus(recipeMapWorkable.isWorkingEnabled(), recipeMapWorkable.isActive())
+                .addEnergyUsageLine(getEnergyContainer())
+                .addEnergyTierLine(GTUtility.getTierByVoltage(recipeMapWorkable.getMaxVoltage()))
+                .addCustom(tl -> {
+                    // Coil energy discount line
+                    if (isStructureFormed()) {
+                        ITextComponent energyDiscount = TextComponentUtil.stringWithColor(TextFormatting.AQUA,
+                                Math.max(0.1, 100 - 10 * coilTier) + "%");
+
+                        ITextComponent base = TextComponentUtil.translationWithColor(
+                                TextFormatting.GRAY,
+                                "gregtech.multiblock.cracking_unit.energy",
+                                energyDiscount);
+
+                        ITextComponent hover = TextComponentUtil.translationWithColor(
+                                TextFormatting.GRAY,
+                                "gregtech.multiblock.cracking_unit.energy_hover");
+
+                        tl.add(TextComponentUtil.setHover(base, hover));
+                    }
+                })
+                .addParallelsLine(recipeMapWorkable.getParallelLimit())
+                .addWorkingStatusLine()
+                .addProgressLine(recipeMapWorkable.getProgressPercent());
+    }
+
     @SideOnly(Side.CLIENT)
     @Override
     public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
         return ZBGTTextures.INCONEL_REINFORCED_CASING;
     }
 
+    protected int getCoilTier() {
+        return coilTier;
+    }
+
     @SuppressWarnings("InnerClassMayBeStatic")
-    private class LargeAlloySmelterRecipeLogic extends MultiblockRecipeLogic {
+    private class LargeAlloySmelterRecipeLogic extends GCYMMultiblockRecipeLogic {
 
         public LargeAlloySmelterRecipeLogic(RecipeMapMultiblockController tileEntity) {
             super(tileEntity);
         }
 
         @Override
-        protected void modifyOverclockPre(int @NotNull [] values, @NotNull IRecipePropertyStorage storage) {
-            super.modifyOverclockPre(values, storage);
+        protected void modifyOverclockPost(int[] overclockResults, @NotNull IRecipePropertyStorage storage) {
+            super.modifyOverclockPost(overclockResults, storage);
 
-            values[1] = (int) (values[1] * Math.pow(0.95, coilTier));
+            int coilTier = ((MetaTileEntityLargeAlloySmelter) metaTileEntity).getCoilTier();
+            if (coilTier <= 0) return;
+
+            overclockResults[0] = (int) Math.max(1, overclockResults[0] * (1.0f - coilTier * 0.1));
         }
     }
 }
