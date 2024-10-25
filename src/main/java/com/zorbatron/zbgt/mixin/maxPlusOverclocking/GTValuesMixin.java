a463f610774d27d4411c6155552087244c9dc5fa
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2024-10-25 19:26:15 -0400

    Yay!

--- src/main/java/com/zorbatron/zbgt/mixin/maxPlusOverclocking/GTValuesMixin.java
+++ src/main/java/com/zorbatron/zbgt/mixin/maxPlusOverclocking/GTValuesMixin.java
@@ -1,18 +1,18 @@
 package com.zorbatron.zbgt.mixin.maxPlusOverclocking;
 
-import gregtech.api.GTValues;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import gregtech.api.GTValues;
 
 @Mixin(value = GTValues.class, remap = false)
 public class GTValuesMixin {
 
-    ///TODO implement this when I grow a brain
-    @Inject(method = "<clinit>", at = @At(value = ""))
-    private static void modifyV(CallbackInfo ci) {
-        long[] newValues = new long[] { 8, 32, 128, 512, 2048, 8192, 32768, 131072, 524288, 2097152, 8388608, 33554432,
-                134217728, 536870912, 2147483648L };
-    }
+    @Shadow
+    @Final
+    @Mutable
+    public static long[] V = new long[] { 8, 32, 128, 512, 2048, 8192, 32768, 131072, 524288, 2097152, 8388608,
+            33554432, 134217728, 536870912, 2147483648L };
 }
