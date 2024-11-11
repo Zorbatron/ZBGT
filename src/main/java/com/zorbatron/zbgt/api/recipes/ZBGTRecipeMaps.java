0d1228b588a2c2a09065acd9092a4e965d677c77
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2024-11-10 20:35:24 -0500

    Remove RecipeIOMod (#85)

--- src/main/java/com/zorbatron/zbgt/api/recipes/ZBGTRecipeMaps.java
+++ src/main/java/com/zorbatron/zbgt/api/recipes/ZBGTRecipeMaps.java
@@ -2,12 +2,8 @@ package com.zorbatron.zbgt.api.recipes;
 
 import static gregtech.api.recipes.RecipeMaps.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.zorbatron.zbgt.api.recipes.builders.CoALRecipeBuilder;
 import com.zorbatron.zbgt.api.recipes.builders.PreciseAssemblerRecipeBuilder;
-import com.zorbatron.zbgt.api.recipes.helpers.RecipeIOMod;
 import com.zorbatron.zbgt.api.recipes.maps.RecipeMapCoAL;
 import com.zorbatron.zbgt.api.recipes.maps.RecipeMapPreciseAssembler;
 
@@ -24,16 +20,9 @@ public final class ZBGTRecipeMaps {
             new PreciseAssemblerRecipeBuilder());
 
     public static void modifyMaps() {
-        List<RecipeIOMod> recipeModList = new ArrayList<>();
-        recipeModList.add(new RecipeIOMod(POLARIZER_RECIPES, 0, 0, 1, 1));
-        recipeModList.add(new RecipeIOMod(COMPRESSOR_RECIPES, 0, 0, 1, 1));
-        // recipeModList.add(new RecipeIOMod(MIXER_RECIPES, 9, 0, 0, 0));
+        POLARIZER_RECIPES.setMaxFluidInputs(1);
+        POLARIZER_RECIPES.setMaxFluidOutputs(1);
 
-        recipeModList.forEach(recipeIOMod -> {
-            recipeIOMod.recipeMap().setMaxInputs(recipeIOMod.minItemInputs());
-            recipeIOMod.recipeMap().setMaxOutputs(recipeIOMod.minItemOutputs());
-            recipeIOMod.recipeMap().setMaxFluidInputs(recipeIOMod.minFluidInputs());
-            recipeIOMod.recipeMap().setMaxFluidOutputs(recipeIOMod.minFluidOutputs());
-        });
+        COMPRESSOR_RECIPES.setMaxFluidInputs(1);
     }
 }
