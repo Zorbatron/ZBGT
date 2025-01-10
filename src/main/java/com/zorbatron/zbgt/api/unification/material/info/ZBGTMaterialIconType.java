38f73570622eb3af6af687774e70e35782088a7a
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-01-10 18:03:50 -0500

    Nanite metaitem (#131)
    * Cherry-pick meta item stuff from nano forge branch
    
    * Plural :tr:
    
    * Textures!!!!
    
    * Lang!!!!!!!!
    
    * Move packages to match base CEu

---
+++ src/main/java/com/zorbatron/zbgt/api/unification/material/info/ZBGTMaterialIconType.java
@@ -0,0 +1,8 @@
+package com.zorbatron.zbgt.api.unification.material.info;
+
+import gregtech.api.unification.material.info.MaterialIconType;
+
+public class ZBGTMaterialIconType {
+
+    public static final MaterialIconType nanites = new MaterialIconType("nanites");
+}
